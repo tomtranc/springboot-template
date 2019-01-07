e8eedca60da32150363a323b2c70dc372210749c
Tom Tran <ttran@fico.com>
2019-01-07 02:42:28 -0800

    Update to deploy using embedded tomcat, fix minor profile issue with docker build

--- docker-resources/bootstrap.sh
+++ docker-resources/bootstrap.sh
@@ -3,4 +3,8 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 cp ${CATALINA_HOME}/logs/bootstrap.logs ${DATA_DIR}/bootstrap${current_time}.log
 
-sh /opt/webserver/bin/launch.sh
+# Deploy using container's webserver
+#sh /opt/webserver/bin/launch.sh
+
+# Deploy using embedded webserver inside jar/war
+java -jar /deployment/ROOT.war
