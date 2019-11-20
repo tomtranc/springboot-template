66d53e887e528408931cc4ed2f59805838687fda
Tom Tran <ttran@fico.com>
2019-11-19 16:57:31 -0800

    Update DTO model string validation format

--- src/main/java/app/model/Customer.java
+++ src/main/java/app/model/Customer.java
@@ -8,8 +8,7 @@ import javax.validation.constraints.Pattern;
 @Data
 public class Customer {
   private String id;
-  @NotNull
-  @Pattern(regexp = "^[a-zA-Z_0-9]+$")
+  @NotNull @Pattern(regexp = "^\\w+$")
   private String name;
   private String description;
 }
